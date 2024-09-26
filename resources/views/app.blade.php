54dcd24846b89b30036287e066b0e5a85044fe41
kristiyan.zhelyazkov <kristiyan.zhelyazkov9@gmail.com>
2024-09-26 17:22:38 +0300

    pwa fixes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,11 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default"> <!-- or 'black-translucent' -->
+
+
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
@@ -12,6 +16,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <link rel="manifest" href="/manifest.json">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
     <!-- Scripts -->
     @routes
