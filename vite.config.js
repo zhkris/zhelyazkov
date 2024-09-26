1c711e2fc3387245e7f3b817c50eede186853889
kristiyan.zhelyazkov <kristiyan.zhelyazkov9@gmail.com>
2024-09-26 17:07:09 +0300

    create PWA

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.ts',
+            input: "resources/js/app.ts",
             refresh: true,
         }),
         vue({
@@ -17,4 +17,8 @@ export default defineConfig({
             },
         }),
     ],
+    build: {
+        manifest: true,
+    },
+    base: "/",
 });
